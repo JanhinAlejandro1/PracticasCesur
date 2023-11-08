ce7be26021492c3eb19d66bae4cc1b4dfb563b2e
JanhinAlejandro1 <janhinalejandro.s375135@cesurformacion.com>
2023-11-08 10:04:32 +0100

    subido abecedario

---
+++ src/abecedario.java
@@ -0,0 +1,19 @@
+
+public class abecedario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+	for (int count=97; count<97+26;count++) {
+		 System.out.print((char)count+" ");
+	}
+	System.out.println();
+     int count=97;
+		do { 
+			System.out.print((char)count+" ");
+			count=count+1;
+		}while (count<97+26);
+		
+	}
+
+}
