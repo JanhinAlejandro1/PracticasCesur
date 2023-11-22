9ea5d45eda02808421a63bda7e7f0fc5c41bb355
JanhinAlejandro1 <janhinalejandro.s375135@cesurformacion.com>
2023-11-22 13:27:21 +0100

    1º arrays

---
+++ src/arrays.java
@@ -0,0 +1,18 @@
+
+public class arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] listaInteger = new int[10];
+		
+		for( int c=0;c<=9; c++) {
+			
+		    listaInteger[c]=c;
+			System.out.println(listaInteger[c]);
+			
+		
+		}
+	}
+
+}
