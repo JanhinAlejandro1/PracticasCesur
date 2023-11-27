3d69acf157c28fca392f7868d58f368433f2c698
JanhinAlejandro1 <janhinalejandro.s375135@cesurformacion.com>
2023-11-27 12:00:16 +0100

    cadena metodo

--- src/Cadena.java
+++ src/Cadena.java
@@ -4,28 +4,27 @@ public class Cadena {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String cadena = "hola mundo";
-
-		for (int contador = 0; contador < cadena.length(); contador++) {
+		String cadena="Hola mundo";
+		int indice=5;
+		for (int contador = 0; contador < indice; contador++) {
 			if (contador % 2 == 0) {
+				System.out.println(cadena.toLowerCase().charAt(contador));
+			} else {
+				System.out.println(cadena.charAt(contador));
+	}
+		}
+	}
+
+	public static void imprime(String cadena, int indice) {
+
+		System.out.println(cadena.charAt(indice));
+
+		for (int contador = 0; contador < indice; contador++) {
+			if (contador % 2 == 0) {
+				System.out.println(cadena.toLowerCase().charAt(contador));
+			} else {
 				System.out.println(cadena.charAt(contador));
 			}
-			else 
-				System.out.println(cadena.toUpperCase().charAt(contador));
-		
-		}
-		cadena=cadena.toUpperCase();
-		System.out.println(cadena);
-		System.out.println("-----------------------------");
-		
-		
-		}
-	
-	
-	
-			public static void imprime (String[]args) {
-				String cadena ="Hola mundo";
-				int indice= 5
-						System.out.println(cadena(indice));
-			}
+		}	
+}
 }
