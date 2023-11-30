71b8a8f071f78fba119f5ffb7b091e6eb20a6c85
JanhinAlejandro1 <janhinalejandro.s375135@cesurformacion.com>
2023-11-30 12:29:28 +0100

    diferentes metodos

---
+++ src/hacedor.java
@@ -0,0 +1,29 @@
+
+public class hacedor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	// devuelve un String que es la unión de los dos parámetros de entrada.
+	static String uneCadenas(String cadena1, String cadena2) {
+		return cadena1 + cadena2;
+	}
+
+	// modifica el String de entrada a mayusculas o minúsculas del segundo parámetro
+	static String pasoCadenaA(String cadena, boolean mayuscula) {
+		if (mayuscula == true) {
+			return cadena.toUpperCase();
+		} else {
+			return cadena.toLowerCase();
+		}
+
+	}
+
+	// devuvelve el número de ocurrencia de un caracater pasado como parámetro en el
+	// STring pasado
+	static int numOcurrenciasChar(String cadena, char caracter) {
+
+	}
+}
