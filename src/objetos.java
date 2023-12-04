8354bb9746402a3923c0eb82622e5e3144d749b9
JanhinAlejandro1 <janhinalejandro.s375135@cesurformacion.com>
2023-12-04 13:29:42 +0100

    objetos

---
+++ src/objetos.java
@@ -0,0 +1,45 @@
+
+public class objetos {
+
+	public static void main(String[] args) {
+
+		String[] cadenaInicial=new String[10];
+		for(int i=0;i<3;i++){
+			cadenaInicial[i]="hola";
+		}
+			listaFijaCadena cadena1;
+			cadena1= new listaFijaCadena (4, cadenaInicial);
+			System.out.println("num elementros:" + cadena1.getNumeroElementos());
+			for (int i=0;i<cadena1.getNumeroElementos();i++) {
+				System.out.println(cadena1.getArray()[i]);
+			// TODO Auto-generated method stub
+		}
+	}
+}
+
+class listaFijaCadena {
+	public listaFijaCadena(int numeroElementos, String[] array) {
+	
+		this.numeroElementos = numeroElementos;
+		this.array = array;
+	}
+	public int getNumeroElementos() {
+		return numeroElementos;
+	}
+	public void setNumeroElementos(int numeroElementos) {
+		this.numeroElementos = numeroElementos;
+	}
+	public String[] getArray() {
+		return array;
+	}
+	public void setArray(String[] array) {
+		this.array = array;
+	}
+	public static int getMaxnum() {
+		return MAXNUM;
+	}
+	private int numeroElementos;
+	private String[] array= new String[10];
+	private static final int MAXNUM = 10;
+
+}
